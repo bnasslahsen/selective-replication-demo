d27a3d285966f60dbdc51a1161562abba9237504
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2022-10-09 11:30:31 +0200

    project init

--- load-policies.sh
+++ load-policies.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-set -a
-source ".env"
-set +a
-
 conjur policy load -b root -f app.yaml
 conjur policy load -b root -f permit-us-replication.yaml
 conjur policy load -b root -f permit-eu-replication.yaml
